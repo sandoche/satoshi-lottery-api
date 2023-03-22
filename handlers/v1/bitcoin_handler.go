614c7589149ea85ee35d3ecfd02493c7671f4876
Sandoche <sandoche@protonmail.com>
2023-03-22 14:44:16 +0000

    New endpoint added

--- handlers/v1/bitcoin_handler.go
+++ handlers/v1/bitcoin_handler.go
@@ -14,3 +14,15 @@ func GetRandomBitCoinPrivateKey(c *gin.Context) {
     "private-key": privateKey,
   })
 }
+
+func GetAddressAndBalanceFromPrivateKey(c *gin.Context) {
+  privateKey := c.Params.ByName("private-key")
+  address := bitcoin_service.GetAddressFromPrivateKey(privateKey)
+  balance, _ := bitcoin_service.GetBalanceFromAddress(address)
+
+  c.JSON(200, gin.H{
+    "private-key": privateKey,
+    "address": address,
+    "balance": balance,
+  })
+}
--- server.go
+++ server.go
@@ -10,6 +10,8 @@ func main() {
   r := gin.Default()
 
   r.GET("/v1/bitcoin", bitcoin_handler.GetRandomBitCoinPrivateKey)
+
+  r.GET("/v1/bitcoin/:private-key", bitcoin_handler.GetAddressAndBalanceFromPrivateKey)
   
   r.Run()
 }
